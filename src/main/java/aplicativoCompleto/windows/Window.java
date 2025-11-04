ddd820a63c8fd636a658ed9610f9b7c6a3d3e202
Evandro Neto <evandro.rsneto@gmail.com>
2025-11-03 23:00:24 -0300

    Refactor Window class to improve table handling and maximize stage on startup

--- src/main/java/aplicativoCompleto/windows/Window.java
+++ src/main/java/aplicativoCompleto/windows/Window.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 import aplicativoCompleto.control.ControladoraProduto;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -21,6 +21,7 @@ import javafx.stage.Stage;
 
 public class Window extends Application {
     private ControladoraProduto controladora;
+    TableView<Map<String, Object>> tabela;
 
     public Window() {
         this.controladora = new ControladoraProduto();
@@ -40,6 +41,7 @@ public class Window extends Application {
             containerInput.getChildren().addAll(lbl, input);
             formulario.getChildren().add(containerInput);
         });
+
         Button btnEnviar = new Button("Enviar");
         btnEnviar.setOnAction(event -> {
             Map<String, String> dadosProduto = new java.util.HashMap<>();
@@ -49,14 +51,18 @@ public class Window extends Application {
                 dadosProduto.put(nomeCampo, valorCampo);
             }
             controladora.adicionar(dadosProduto);
+            tabela.setItems(FXCollections.observableList(controladora.listar()));
+            tabela.refresh();
         });
+
         formulario.getChildren().add(btnEnviar);
+
         return formulario;
     }
 
     private VBox criarTabela() {
         VBox containerTabela = new VBox(10);
-        TableView<Map<String, Object>> tabela = new TableView<>();
+        tabela = new TableView<>();
 
         controladora.camposListados().forEach((label, campo) -> {
             TableColumn<Map<String, Object>, Object> coluna = new TableColumn<>(label);
@@ -64,10 +70,7 @@ public class Window extends Application {
             tabela.getColumns().add(coluna);
         });
 
-        // Dados
-        ObservableList<Map<String, Object>> dados = FXCollections.observableList(controladora.listar());
-
-        tabela.setItems(dados);
+        tabela.setItems(FXCollections.observableList(controladora.listar()));
         containerTabela.getChildren().add(tabela);
         return containerTabela;
     }
@@ -77,10 +80,12 @@ public class Window extends Application {
         VBox root = new VBox(20, criarTabela(), criarFormulario());
         root.setPadding(new Insets(40));
 
-        Scene scene = new Scene(root, 640, 480);
-
-        stage.setTitle("Aplicativo Completo");
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Aplicativo Completo");
+        stage.setResizable(true);
         stage.show();
+
+        Platform.runLater(() -> stage.setMaximized(true));
     }
 }
